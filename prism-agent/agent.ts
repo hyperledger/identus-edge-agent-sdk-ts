fb342c73c3639b5568d7a34ccac96eb866184363
Ribó <elribonazo@gmail.com>
2023-03-01 19:20:34 +0100

    feat: implement the agent createPeerDID method.

--- prism-agent/agent.ts
+++ prism-agent/agent.ts
@@ -2,7 +2,10 @@ import Apollo from "../domain/buildingBlocks/Apollo";
 import {
   Service as DIDDocumentService,
   ServiceEndpoint as DIDDocumentServiceEndpoint,
+  VerificationMethod as DIDDocumentVerificationMethod,
+  VerificationMethods as DIDDocumentVerificationMethods,
   Seed,
+  DIDDocumentCoreProperty,
   Signature,
   DID,
   Curve,
@@ -21,7 +24,6 @@ import {
   PrismOnboardingInvitation,
 } from "./types";
 import { AgentError } from "../domain/models/Errors";
-import { connect } from "http2";
 
 enum AgentState {
   STOPPED,
@@ -125,7 +127,64 @@ export default class Agent {
     services: DIDDocumentService[] = [],
     updateMediator: boolean = true
   ): Promise<DID> {
-    throw new Error("Not implemented");
+    const index = this.pluto.getPrismLastKeyPathIndex();
+    const keyAgreementKeyPair = this.apollo.createKeyPairFromKeyCurve(
+      this.seed,
+      {
+        curve: Curve.X25519,
+        index: index,
+      }
+    );
+    const authenticationKeyPair = this.apollo.createKeyPairFromKeyCurve(
+      this.seed,
+      {
+        curve: Curve.ED25519,
+        index: index,
+      }
+    );
+
+    const did = await this.castor.createPeerDID(
+      [keyAgreementKeyPair, authenticationKeyPair],
+      services
+    );
+
+    if (updateMediator) {
+      //TODO(): This still needs to be done update the key List
+    }
+
+    this.pluto.storePeerDID(did, [
+      keyAgreementKeyPair.privateKey,
+      authenticationKeyPair.privateKey,
+    ]);
+
+    // The next logic is a bit tricky, so it's not forgotten this is a reminder.
+    // The next few lines are needed because of DIDComm library, the library will need
+    // to get the secret(private key) that is pair of the public key within the DIDPeer Document
+    // to this end the library will give you the id of the public key that is `did:{method}:{methodId}#ecnumbasis`.
+    // So the code below after the did is created, it will retrieve the document and
+    // and store the private keys with the corresponding `id` of the one created on the document.
+    // So when the secret resolver asks for the secret we can identify it.
+    const didDocument = await this.castor.resolveDID(did.toString());
+
+    const verificationMethods = didDocument.coreProperties.reduce<
+      DIDDocumentVerificationMethod[]
+    >((result, property) => {
+      if (property instanceof DIDDocumentVerificationMethods) {
+        result.push(...property.values);
+      }
+      return result;
+    }, []);
+
+    verificationMethods.forEach((verificationMethod, i) => {
+      const privateKey =
+        verificationMethod.type.indexOf("X25519") !== -1
+          ? keyAgreementKeyPair.privateKey
+          : authenticationKeyPair.privateKey;
+
+      this.pluto.storePrivateKeys(privateKey, did, i, verificationMethod.id);
+    });
+
+    return did;
   }
 
   async parseInvitation(str: string): Promise<InvitationType> {
