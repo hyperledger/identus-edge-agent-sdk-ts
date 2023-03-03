467a5c3f85b6c453b8a014138258c5a628b10bda
Milos Dzepina <milos.dzepina@iohk.io>
2023-03-03 17:31:14 +0100

    feat(build): Simplify SDK library build configuration (#11)

--- examples/browser-ts/src/App.tsx
+++ examples/browser-ts/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as sdk from "@input-output-hk/atala-prism-wallet-sdk/browser";
+import * as sdk from "@input-output-hk/atala-prism-wallet-sdk";
 
 const apollo = new sdk.Apollo();
 console.log('Apollo test:', apollo.createRandomSeed());
