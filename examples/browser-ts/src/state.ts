9027de09cc7dbf530a7ed20b7d8f7c05b2f59071
Milos Dzepina <milos.dzepina@iohk.io>
2023-03-07 12:25:58 +0100

    remove unused code

--- examples/browser-ts/src/state.ts
+++ examples/browser-ts/src/state.ts
@@ -4,7 +4,3 @@ import { Domain } from "@input-output-hk/atala-prism-wallet-sdk";
 export const mnemonicsAtom = atom<Domain.MnemonicWordList | undefined>(
   undefined
 );
-export const keyPairAtom = atom<Domain.KeyPair | undefined>(undefined);
-export const keyPairSecp256Atom = atom<Domain.KeyPair | undefined>(undefined);
-export const keyPairEd25519Atom = atom<Domain.KeyPair | undefined>(undefined);
-export const keyPairX25519Atom = atom<Domain.KeyPair | undefined>(undefined);
