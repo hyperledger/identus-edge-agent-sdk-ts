55839a055e735a9d176a326e823815a6e854b577
Ianos <ianos.dorultan@iohk.io>
2023-03-08 11:47:47 +0200

    fix: eslint connection issues

--- pluto/Connection.ts
+++ pluto/Connection.ts
@@ -27,62 +27,44 @@ export default class Connection implements ConnectionModel {
 
   private get SQLDatabase(): Promise<SQLDatabaseType> {
 
-    return new Promise(async (resolve, reject) => {
-      try {
-        const sqlInit: InitSqlJsStatic = await this.getSQLPackage();
-        const that = this;
-        const SQL = await sqlInit({
+    return new Promise((resolve, reject) => {
+      this.getSQLPackage().then((sqlInit: InitSqlJsStatic) => {
+        sqlInit({
           // In browser should load async from URL
-          locateFile: file => that.wasmBinaryURL ?? `https://sql.js.org/dist/${file}`
-        });
+          locateFile: (file: string) => this.wasmBinaryURL ?? `https://sql.js.org/dist/${file}`
+        }).then(SQL => resolve(new SQL.Database(this.sqliteDatabase))).catch(reject);
+      });
 
-        resolve(new SQL.Database(this.sqliteDatabase));
-
-      } catch (error) {
-        reject(error);
-      }
     });
   }
 
   private get SqliteDatabase(): Promise<SQLiteDatabaseType> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const sqlitePackage = await this.getSqlitePackage();
+    return new Promise((resolve, reject) => {
+      this.getSqlitePackage().then(sqlitePackage => {
         const sqlite3 = sqlitePackage.verbose();
-        resolve(new sqlite3.Database(this.filename ?? ":memory")); // default to memory
-      } catch (error) {
-        reject(error);
-      }
+        resolve(new sqlite3.Database(this.filename ?? ":memory:"));
+      }).catch(reject);
     });
   }
 
   async connect(): Promise<any> {
     switch (this.type) {
       case 'sql':
-        try {
-          this.database = await this.SQLDatabase;
-          this.connected = true;
-        } catch (error) {
-          throw error;
-        }
+        this.database = await this.SQLDatabase;
+        this.connected = true;
         break;
       case 'sqlite':
-        try {
-          const db = await this.SqliteDatabase;
-          this.database = db;
-          this.connected = await new Promise<boolean>((resolve, reject) => {
-            const openHandler = () => {
-              resolve(true);
-            };
-            const errorHandler = () => {
-              reject(false);
-            };
-            db.on("open", openHandler);
-            db.on('error', errorHandler);
-          });
-        } catch (error) {
-          throw error;
-        }
+        this.database = await this.SqliteDatabase;
+        this.connected = await new Promise<boolean>((resolve, reject) => {
+          const openHandler = () => {
+            resolve(true);
+          };
+          const errorHandler = () => {
+            reject(false);
+          };
+          (this.database as SQLiteDatabaseType).on("open", openHandler);
+          (this.database as SQLiteDatabaseType).on('error', errorHandler);
+        });
         break;
     }
   }
@@ -105,16 +87,17 @@ export default class Connection implements ConnectionModel {
   }
 
   transformResponseToObject(values: any, columns: any) {
-    let object: { [key: string]: string | number | null } = {};
-    for (let key in columns) {
+    const object: { [key: string]: string | number | null } = {};
+    for (const key in columns) {
       object[columns[key]] = values[key];
     }
     return object;
   }
 
   execAsOne<param>(query: string, params?: ParamsType[] | { [key: string]: ParamsType }): param | null | Promise<param | null> {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    let result = this.exec<param>(query, params, true) as any;
+    const result = this.exec<param>(query, params, true) as any;
     if (result instanceof Promise) {
       return result;
     }
@@ -126,8 +109,9 @@ export default class Connection implements ConnectionModel {
   }
 
   execAsMany<param>(query: string, params?: ParamsType[] | { [key: string]: ParamsType }): param[] | Promise<param[]> {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    let result = this.exec<param>(query, params) as any;
+    const result = this.exec<param>(query, params) as any;
     if (result instanceof Array) {
       return result[0].values.map((values: any) => this.transformResponseToObject(values, result[0].columns));
     } else {
