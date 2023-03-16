30de58b3e8b69e15350ed3d7d71f3ca552b676a6
Ribó <elribonazo@gmail.com>
2023-03-16 20:48:04 +0100

    Add TODO

--- prism-agent/mediator/PlutoMediatorStore.ts
+++ prism-agent/mediator/PlutoMediatorStore.ts
@@ -6,11 +6,16 @@ export class PublicMediatorStore implements MediatorStore {
   constructor(private pluto: Pluto) {}
 
   async storeMediator(mediator: Mediator): Promise<void> {
-    return this.pluto.storeMediator(
+    const response = this.pluto.storeMediator(
       mediator.mediatorDID,
       mediator.hostDID,
       mediator.routingDID
     );
+    //TODO: FIX THIS, how can the mediator be empty if we just inserted it.
+    const mediators = await this.getAllMediators();
+    console.log(mediators);
+    debugger;
+    return response;
   }
 
   async getAllMediators(): Promise<Mediator[]> {
