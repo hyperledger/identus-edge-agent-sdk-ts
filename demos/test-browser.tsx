e332996fe27a8be6799c3037072b412eb33ce556
Ianos <dorultanianosgyorgy@gmail.com>
2023-03-20 11:54:35 +0200

    fix: browser webpack issue (#29)
    Signed-off-by: Francisco Javier Ribó Labrador <elribonazo@gmail.com>

--- demos/test-browser.tsx
+++ demos/test-browser.tsx
@@ -1,12 +1,27 @@
-import React, { StrictMode} from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./browser/App";
 
+console.log("Something from here");
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+if (module.hot) {
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+  module.hot.accept('./browser/App', () => {
+    try {
+      // Any code that triggers a HMR update
+    } catch (error) {
+      console.error('Error in the hot update:', error);
+    }
+  });
+}
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-    <App></App>
+    <App/>
 );
 
 // import * as SDK from "..";
--- mercury/forward/ForwardMessage.ts
+++ mercury/forward/ForwardMessage.ts
@@ -1,5 +1,5 @@
-import { uuid } from "@stablelib/uuid";
-import { AttachmentDescriptor, Message } from "../../domain";
+import {uuid} from "@stablelib/uuid";
+import {AttachmentDescriptor, DID, Message} from "../../domain";
 
 export interface ForwardMessageBody {
   next: string;
@@ -9,18 +9,19 @@ export class ForwardMessage {
   public static type = "https://didcomm.org/routing/2.0/forward";
 
   constructor(
-    public body: ForwardMessageBody,
-    public from: DID,
-    public to: DID,
-    public encryptedMessage: string,
-    public id: string = uuid()
-  ) {}
+      public body: ForwardMessageBody,
+      public from: DID,
+      public to: DID,
+      public encryptedMessage: string,
+      public id: string = uuid()
+  ) {
+  }
 
   makeMessage(): Message {
     const body = JSON.stringify(this.body);
     const attachment = new AttachmentDescriptor(
-      { data: this.encryptedMessage },
-      "application/json"
+        {data: this.encryptedMessage},
+        "application/json"
     );
     return new Message(body, this.id, ForwardMessage.type, this.from, this.to, [
       attachment,
