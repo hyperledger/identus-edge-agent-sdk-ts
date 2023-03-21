d32211f29ba9deca0fec2b21f73ae74a6e36fb5d
Ianos <ianos.dorultan@iohk.io>
2023-03-21 16:23:53 +0200

    fix: basic message export

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-export { default as Apollo } from "./apollo/Apollo";
-export { default as Castor } from "./castor/Castor";
+export {default as Apollo} from "./apollo/Apollo";
+export {default as Castor} from "./castor/Castor";
 export * as Domain from "./domain";
-export { default as Mercury } from "./mercury/Mercury";
-export { default as Pluto } from "./pluto/Pluto";
-export { default as Pollux } from "./pollux/Pollux";
-export { default as Agent } from "./prism-agent/Agent";
+export {default as Mercury} from "./mercury/Mercury";
+export {default as Pluto} from "./pluto/Pluto";
+export {default as Pollux} from "./pollux/Pollux";
+export {default as Agent} from "./prism-agent/Agent";
+export * from "./prism-agent/protocols/other/BasicMessage";
 export * from "./prism-agent/connectionsManager/ConnectionsManager";
 export * from "./prism-agent/mediator/BasicMediatorHandler";
 export * from "./prism-agent/mediator/PlutoMediatorStore";
