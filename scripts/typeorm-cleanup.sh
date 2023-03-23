bbd2c64f5172d9724540f5f288e6b55568cb383c
Ianos <ianos.dorultan@iohk.io>
2023-03-23 11:42:55 +0200

    chore: add comment

--- scripts/typeorm-cleanup.sh
+++ scripts/typeorm-cleanup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// In order to fix the react-native-sqlite-storage complain from typeorm/browser,
+// we need this script to run after installing typeorm.
 const replace = require('replace');
 const path = require('path');
 
