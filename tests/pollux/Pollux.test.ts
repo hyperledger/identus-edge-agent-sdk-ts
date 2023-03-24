716fff734c668255d0d744c81000405cada10efb
Ribó <elribonazo@gmail.com>
2023-03-24 12:06:27 +0100

    Apply latest improvements and changes in types into tests.

--- tests/pollux/Pollux.test.ts
+++ tests/pollux/Pollux.test.ts
@@ -2,14 +2,20 @@ import { expect } from "chai";
 import { DID } from "../../domain";
 import Castor from "../../castor/Castor";
 import Apollo from "../../domain/buildingBlocks/Apollo";
-import { InvalidCredentialError, InvalidJWTString } from "../../domain/models/errors/Pollux";
-import { CredentialType, VerifiableCredential, VerifiableCredentialTypeContainer } from "../../domain/models/VerifiableCredential";
+import {
+  InvalidCredentialError,
+  InvalidJWTString,
+} from "../../domain/models/errors/Pollux";
+import {
+  CredentialType,
+  VerifiableCredential,
+} from "../../domain/models/VerifiableCredential";
 import Pollux from "../../pollux/Pollux";
 
 const jwtParts = [
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
   "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwidHlwZSI6Imp3dCJ9",
-  "18bn-r7uRWAG4FCFBjxemKvFYPCAoJTOHaHthuXh5nM"
+  "18bn-r7uRWAG4FCFBjxemKvFYPCAoJTOHaHthuXh5nM",
 ];
 const jwtString = jwtParts.join(".");
 
@@ -24,23 +30,29 @@ describe("Pollux", () => {
 
   describe("parseVerifiableCredential", () => {
     describe("Invalid JWT string", () => {
-      [
-        "",
-        `${jwtParts[0]}`,
-        `${jwtParts[0]}.${jwtParts[1]}`,
-      ].forEach(value => {
-        it(`should error when too few parts [${value.split(".").length}]`, () => {
-          expect(() => pollux.parseVerifiableCredential(value)).throws(InvalidJWTString);
-        })
-      });
+      ["", `${jwtParts[0]}`, `${jwtParts[0]}.${jwtParts[1]}`].forEach(
+        (value) => {
+          it(`should error when too few parts [${
+            value.split(".").length
+          }]`, () => {
+            expect(() => pollux.parseVerifiableCredential(value)).throws(
+              InvalidJWTString
+            );
+          });
+        }
+      );
 
       [
         `${jwtString}.${jwtParts[0]}`,
         `${jwtString}.${jwtParts[0]}.${jwtParts[1]}`,
-      ].forEach(value => {
-        it(`should error when too many parts [${value.split(".").length}]`, () => {
-          expect(() => pollux.parseVerifiableCredential(value)).throws(InvalidJWTString);
-        })
+      ].forEach((value) => {
+        it(`should error when too many parts [${
+          value.split(".").length
+        }]`, () => {
+          expect(() => pollux.parseVerifiableCredential(value)).throws(
+            InvalidJWTString
+          );
+        });
       });
 
       it("should error when not encoded JSON", () => {
@@ -48,7 +60,7 @@ describe("Pollux", () => {
         const value = `${jwtParts[0]}.${encoded}.${jwtParts[2]}`;
 
         expect(() => pollux.parseVerifiableCredential(value)).throws();
-      })
+      });
     });
 
     const encodeCredential = (cred: object): string => {
@@ -56,12 +68,12 @@ describe("Pollux", () => {
       const encoded = Buffer.from(json).toString("base64");
       const value = `${jwtParts[0]}.${encoded}.${jwtParts[2]}`;
       return value;
-    }
+    };
 
     const makeCredentialForType = (type: string[]): string => {
       const cred: Pick<VerifiableCredential, "type"> = { type };
       return encodeCredential(cred);
-    }
+    };
 
     describe("Invalid Credential", () => {
       [
@@ -71,18 +83,24 @@ describe("Pollux", () => {
       ].forEach((type: string[]) => {
         it(`should error with incorrect Credential.type [${type}]`, () => {
           const value = makeCredentialForType(type);
-          expect(() => pollux.parseVerifiableCredential(value)).throws(InvalidCredentialError);
+          expect(() => pollux.parseVerifiableCredential(value)).throws(
+            InvalidCredentialError
+          );
         });
       });
 
       it(`should error with incorrect case Credential.type [JWT]`, () => {
         const value = makeCredentialForType(["JWT"]);
-        expect(() => pollux.parseVerifiableCredential(value)).throws(InvalidCredentialError);
+        expect(() => pollux.parseVerifiableCredential(value)).throws(
+          InvalidCredentialError
+        );
       });
 
       it(`should error with incorrect case Credential.type [W3C]`, () => {
         const value = makeCredentialForType(["W3C"]);
-        expect(() => pollux.parseVerifiableCredential(value)).throws(InvalidCredentialError);
+        expect(() => pollux.parseVerifiableCredential(value)).throws(
+          InvalidCredentialError
+        );
       });
     });
 
@@ -94,21 +112,42 @@ describe("Pollux", () => {
           type: [CredentialType.JWT],
           aud: ["aud"],
           context: ["context"],
-          credentialSubject: "credSubject",
-          evidence: new VerifiableCredentialTypeContainer("evidenceId", "evidenceType"),
-          expirationDate: "expDate",
-          issuanceDate: "issDate",
+          credentialSubject: { whatever: "credSubject" },
+          evidence: {
+            id: "evidenceId",
+            type: "evidenceType",
+          },
+          expirationDate: 1324567,
+          issuanceDate: 2345678,
           issuer: new DID(
             "did",
             "peer",
             "2.Ez6LSms555YhFthn1WV8ciDBpZm86hK9tp83WojJUmxPGk1hZ.Vz6MkmdBjMyB4TS5UbbQw54szm8yvMMf1ftGV2sQVYAxaeWhE.SeyJpZCI6Im5ldy1pZCIsInQiOiJkbSIsInMiOiJodHRwczovL21lZGlhdG9yLnJvb3RzaWQuY2xvdWQiLCJhIjpbImRpZGNvbW0vdjIiXX0"
           ),
-          refreshService: new VerifiableCredentialTypeContainer("refreshServiceId", "refreshServiceType"),
-          termsOfUse: new VerifiableCredentialTypeContainer("termsOfUseId", "termsOfUseType"),
-          validFrom: new VerifiableCredentialTypeContainer("validFromId", "validFromType"),
-          validUntil: new VerifiableCredentialTypeContainer("validUntilId", "validUntilType"),
-          credentialSchema: new VerifiableCredentialTypeContainer("credentialSchemaId", "credentialSchemaType"),
-          credentialStatus: new VerifiableCredentialTypeContainer("credentialStatusId", "credentialStatusType"),
+          refreshService: {
+            id: "refreshServiceId",
+            type: "refreshServiceType",
+          },
+          termsOfUse: {
+            id: "termsOfUseId",
+            type: "termsOfUseType",
+          },
+          validFrom: {
+            id: "validFromId",
+            type: "validFromType",
+          },
+          validUntil: {
+            id: "validUntilId",
+            type: "validUntilType",
+          },
+          credentialSchema: {
+            id: "credentialSchemaId",
+            type: "credentialSchemaType",
+          },
+          credentialStatus: {
+            id: "credentialStatusId",
+            type: "credentialStatusType",
+          },
           proof: "proof",
         };
 
@@ -129,27 +168,26 @@ describe("Pollux", () => {
         expect(result.issuer).to.be.an.instanceOf(DID);
         expect(result.issuer).to.eql(cred.issuer);
 
-        expect(result.evidence).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.evidence).to.be.deep.equal(cred.evidence);
         expect(result.evidence).to.eql(cred.evidence);
 
-        expect(result.refreshService).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.refreshService).to.deep.equal(cred.refreshService);
         expect(result.refreshService).to.eql(cred.refreshService);
 
-        expect(result.termsOfUse).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.termsOfUse).to.deep.equal(cred.termsOfUse);
         expect(result.termsOfUse).to.eql(cred.termsOfUse);
 
-        expect(result.validFrom).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.validFrom).to.deep.equal(cred.validFrom);
         expect(result.validFrom).to.eql(cred.validFrom);
 
-        expect(result.validUntil).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.validUntil).to.deep.equal(cred.validFrom);
         expect(result.validUntil).to.eql(cred.validUntil);
 
-        expect(result.credentialSchema).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.credentialSchema).to.deep.equal(cred.validUntil);
         expect(result.credentialSchema).to.eql(cred.credentialSchema);
 
-        expect(result.credentialStatus).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.credentialStatus).to.deep.equal(cred.credentialStatus);
         expect(result.credentialStatus).to.eql(cred.credentialStatus);
-
       });
 
       it(`should return W3CVerifiableCredential`, () => {
@@ -159,21 +197,42 @@ describe("Pollux", () => {
           type: [CredentialType.W3C],
           aud: ["aud"],
           context: ["context"],
-          credentialSubject: "credSubject",
-          evidence: new VerifiableCredentialTypeContainer("evidenceId", "evidenceType"),
-          expirationDate: "expDate",
-          issuanceDate: "issDate",
+          credentialSubject: { whatever: "credSubject" },
+          evidence: {
+            id: "evidenceId",
+            type: "evidenceType",
+          },
+          expirationDate: 1324567,
+          issuanceDate: 2345678,
           issuer: new DID(
             "did",
             "peer",
             "2.Ez6LSms555YhFthn1WV8ciDBpZm86hK9tp83WojJUmxPGk1hZ.Vz6MkmdBjMyB4TS5UbbQw54szm8yvMMf1ftGV2sQVYAxaeWhE.SeyJpZCI6Im5ldy1pZCIsInQiOiJkbSIsInMiOiJodHRwczovL21lZGlhdG9yLnJvb3RzaWQuY2xvdWQiLCJhIjpbImRpZGNvbW0vdjIiXX0"
           ),
-          refreshService: new VerifiableCredentialTypeContainer("refreshServiceId", "refreshServiceType"),
-          termsOfUse: new VerifiableCredentialTypeContainer("termsOfUseId", "termsOfUseType"),
-          validFrom: new VerifiableCredentialTypeContainer("validFromId", "validFromType"),
-          validUntil: new VerifiableCredentialTypeContainer("validUntilId", "validUntilType"),
-          credentialSchema: new VerifiableCredentialTypeContainer("credentialSchemaId", "credentialSchemaType"),
-          credentialStatus: new VerifiableCredentialTypeContainer("credentialStatusId", "credentialStatusType"),
+          refreshService: {
+            id: "refreshServiceId",
+            type: "refreshServiceType",
+          },
+          termsOfUse: {
+            id: "termsOfUseId",
+            type: "termsOfUseType",
+          },
+          validFrom: {
+            id: "validFromId",
+            type: "validFromType",
+          },
+          validUntil: {
+            id: "validUntilId",
+            type: "validUntilType",
+          },
+          credentialSchema: {
+            id: "credentialSchemaId",
+            type: "credentialSchemaType",
+          },
+          credentialStatus: {
+            id: "credentialStatusId",
+            type: "credentialStatusType",
+          },
           proof: "proofW3c",
         };
 
@@ -194,29 +253,27 @@ describe("Pollux", () => {
         expect(result.issuer).to.be.an.instanceOf(DID);
         expect(result.issuer).to.eql(cred.issuer);
 
-        expect(result.evidence).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.evidence).to.be.deep.equal(cred.evidence);
         expect(result.evidence).to.eql(cred.evidence);
 
-        expect(result.refreshService).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.refreshService).to.deep.equal(cred.refreshService);
         expect(result.refreshService).to.eql(cred.refreshService);
 
-        expect(result.termsOfUse).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.termsOfUse).to.deep.equal(cred.termsOfUse);
         expect(result.termsOfUse).to.eql(cred.termsOfUse);
 
-        expect(result.validFrom).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.validFrom).to.deep.equal(cred.validFrom);
         expect(result.validFrom).to.eql(cred.validFrom);
 
-        expect(result.validUntil).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.validUntil).to.deep.equal(cred.validFrom);
         expect(result.validUntil).to.eql(cred.validUntil);
 
-        expect(result.credentialSchema).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.credentialSchema).to.deep.equal(cred.validUntil);
         expect(result.credentialSchema).to.eql(cred.credentialSchema);
 
-        expect(result.credentialStatus).to.be.an.instanceOf(VerifiableCredentialTypeContainer);
+        expect(result.credentialStatus).to.deep.equal(cred.credentialStatus);
         expect(result.credentialStatus).to.eql(cred.credentialStatus);
-
       });
     });
   });
-
 });
