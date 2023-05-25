3b662b1be7d44d9c3a6e0ec9873b4f93bc3399b8
Ribó <elribonazo@gmail.com>
2023-05-25 11:48:05 +0200

    replace bip32 with a compatible package.

--- src/globals.ts
+++ src/globals.ts
@@ -1,1 +1,7 @@
 import SQL from "sql.js/dist/sql-wasm.js";
+import localforage from "localforage";
+
+if (typeof window !== "undefined") {
+  (window as any).SQL = SQL;
+  (window as any).localforage = localforage;
+}
