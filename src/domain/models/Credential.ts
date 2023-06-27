bb6d67f5185765ecb579d878ba28ed4e158daf90
Ribó <elribonazo@gmail.com>
2023-06-27 16:08:28 +0200

    wip interface

--- src/domain/models/Credential.ts
+++ src/domain/models/Credential.ts
@@ -1,24 +1,28 @@
 import { CredentialType } from "./VerifiableCredential";
 
-abstract class Credential {
-  abstract credentialType: CredentialType;
-  abstract issuerId: string;
-  abstract subjectId: string;
-
-  abstract credentialCreated?: string;
-  abstract credentialUpdated?: string;
-  abstract validUntil?: string;
-
-  abstract revoked?: boolean;
-
-  abstract claims: Map<string, string>;
-  abstract properties: Map<CredentialProperties | string, any>;
+interface Claim {
+  [name: string]: any;
 }
 
-abstract class StorableCredential {
-  abstract id: string;
-  abstract recoveryId: string;
-  abstract credentialData: Uint8Array;
+abstract class Credential {
+  abstract issuer: string;
+  abstract subject: string;
+  abstract claims: Claim[];
+  abstract properties: Map<string, any>;
+}
+
+interface StorableCredential {
+  id: string;
+  recoveryId: string;
+  credentialData: Uint8Array;
+  issuer?: string;
+  subject?: string;
+  credentialCreated?: Date;
+  credentialUpdated?: Date;
+  credentialSchema?: string;
+  validUntil?: Date;
+  revoqued?: boolean;
+  availableClaims?: string[];
 }
 
 /*
@@ -35,79 +39,13 @@ CREATE TABLE Credential (
     revoked INTEGER AS Int DEFAULT 0,
     PRIMARY KEY (id)
 );
+CREATE TABLE AvailableClaims (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    credentialId TEXT NOT NULL,
+    claim TEXT NOT NULL,
+    FOREIGN KEY (credentialId) REFERENCES Credential(id)
+);
+CREATE TABLE LinkSecret (
+    id TEXT NOT NULL UNIQUE
+);
 */
-
-/**
- * The generic properties that all the credentials have
- */
-enum CredentialProperties {
-  field = "field",
-}
-
-enum JWTVerifiableCredentialProperties {
-  iss = "iss",
-  jti = "jti",
-  nbf = "nbf",
-  aud = "aud",
-  sub = "sub",
-}
-
-class JWTVerifiablePayload extends StorableCredential implements Credential {
-  constructor(
-    public id: string = "TODO: Generated uuid v4",
-    public recoveryId: string = "TODO: Generated uuid v4, how is this generated, defined by user?",
-    public credentialType: CredentialType,
-    public issuerId: string,
-    public subjectId: string,
-    public claims: Map<string, string>,
-    public properties: Map<
-      CredentialProperties | JWTVerifiableCredentialProperties | string,
-      any
-    >,
-    public credentialCreated?: string | undefined,
-    public credentialUpdated?: string | undefined,
-    public validUntil?: string | undefined,
-    public revoked?: boolean | undefined
-  ) {
-    super();
-  }
-
-  get credentialData() {
-    return Buffer.from(JSON.stringify(this));
-  }
-
-  static fromBuffer(dbEntry: any): JWTVerifiablePayload {
-    const credentialData = dbEntry.credentialData;
-
-    const properties: Map<
-      CredentialProperties | JWTVerifiableCredentialProperties | string,
-      any
-    > = new Map();
-
-    const decodedObject = JSON.parse(Buffer.from(credentialData).toString());
-
-    if (decodedObject[JWTVerifiableCredentialProperties.aud]) {
-      properties.set(
-        JWTVerifiableCredentialProperties.aud,
-        decodedObject[JWTVerifiableCredentialProperties.aud]
-      );
-    }
-
-    if (decodedObject[JWTVerifiableCredentialProperties.iss]) {
-      properties.set(
-        JWTVerifiableCredentialProperties.iss,
-        decodedObject[JWTVerifiableCredentialProperties.iss]
-      );
-    }
-
-    return new JWTVerifiablePayload(
-      "",
-      "",
-      CredentialType.JWT,
-      "",
-      "",
-      new Map(),
-      properties
-    );
-  }
-}
