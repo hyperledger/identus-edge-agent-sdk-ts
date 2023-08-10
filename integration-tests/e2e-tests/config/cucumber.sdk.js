a9d12e72e403cda18b9885174bd05de093f84e66
Allain Magyar <101114634+amagyar-iohk@users.noreply.github.com>
2023-08-10 15:08:17 +0100

    test: add e2e tests (#76)

---
+++ integration-tests/e2e-tests/config/cucumber.sdk.js
@@ -0,0 +1,13 @@
+module.exports = {
+  default: [
+    `--publish-quiet`,
+    `--format '@serenity-js/cucumber'`,
+    `--require=tests/sdk/steps/**/*.ts`,
+    `--require=config/serenity.config.ts`,
+    `--require-module=ts-node/register`,
+    '--backtrace'
+  ].join(' '),
+  glue: [
+    "tests/sdk/src/**/*.ts"
+  ]
+}
