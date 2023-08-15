912998203338eb667cc2790989fc4d48b1e2c469
Ribó <elribonazo@gmail.com>
2023-08-15 18:01:17 +0200

    Add demos to Mock anoncreds issue + verification.

---
+++ .mock/dist/index.d.ts
@@ -0,0 +1,4 @@
+import * as SDK from "@input-output-hk/atala-prism-wallet-sdk";
+export declare const issuerDID: SDK.Domain.DID;
+export declare const credentialSchemaId: string;
+export declare const credentialDefinitionId: string;
