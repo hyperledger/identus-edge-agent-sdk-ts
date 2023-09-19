e0b58c0703636928d712a0e2a5c88223a08fa4e9
Ribó <elribonazo@gmail.com>
2023-09-19 17:37:16 +0200

    WIP add required configuration to allow apollo to run under jest.

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    presets: [
-        "@babel/preset-typescript",
-        [
-            "@babel/preset-env",
-            {
-                useBuiltIns: "usage",
-                corejs: 3,
-            },
-        ],
-        "@babel/preset-react",
+  presets: [
+    "@babel/preset-typescript",
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: 3,
+      },
     ],
-    plugins: [
-        ["@babel/plugin-transform-typescript"],
-        "babel-plugin-transform-typescript-metadata",
-        ["@babel/plugin-proposal-decorators", {"legacy": true}],
-        ["@babel/plugin-proposal-class-properties", {"loose": false}],
-    ],
+    "@babel/preset-react",
+  ],
+  plugins: [
+    ["@babel/plugin-transform-typescript"],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: false }],
+  ],
 };
