caef9228a17849c6dd677773d8c965b0295debc9
Curtish <ch@curtish.me>
2023-10-12 12:28:42 +0100

    update jest for anoncreds

--- tests/setupJest.js
+++
@@ -1,7 +0,0 @@
-// jest requires this for scoping in mock fns
-import mockfs from "fs";
-
-jest.mock(
-  "anoncreds/anoncreds_bg.wasm",
-  () => () => mockfs.readFileSync(`${process.cwd()}/../anoncreds-rs/pkg/anoncreds_bg.wasm`)
-);
