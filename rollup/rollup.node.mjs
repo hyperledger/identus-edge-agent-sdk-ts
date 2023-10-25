6a10c22983264eacf6696f402e3bd6ed8ca15a96
Curtis <curtis.harding@iohk.io>
2023-10-25 16:13:31 +0100

    fix: remove unused packages and fix node demo (#99)
    Signed-off-by: Francisco Javier Ribó Labrador <elribonazo@gmail.com>

--- rollup/rollup.node.mjs
+++ rollup/rollup.node.mjs
@@ -1,3 +1,13 @@
 import Base from "./base.mjs";
 
-export default Base("node", []);
+export default Base("node", [
+  {
+    name: "inject-crypto-global",
+    banner() {
+      return `
+        const crypto = require("crypto");
+        global.crypto = crypto;
+      `;
+    }
+  },
+]);
