10de32891d93273c844804937d9613d9eb1a11de
Francisco Javier Ribó Labrador <elribonazo@gmail.com>
2023-12-08 17:46:59 +0100

    fix: remove uneeded export.

--- src/index.ts
+++ src/index.ts
@@ -40,4 +40,3 @@ export * from "./apollo/utils/X25519KeyPair";
 export * from "./pollux/models/AnonCredsVerifiableCredential";
 export * from "./pollux/models/JWTVerifiableCredential";
 export { KeyProperties } from "./domain/models/KeyProperties";
-export * from './pollux/AnoncredsLoader';
