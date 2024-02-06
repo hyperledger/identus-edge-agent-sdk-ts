09cab819b19be948edd30b7aa9edc641f9add0d7
Francisco Javier Ribó Labrador <elribonazo@gmail.com>
2024-02-06 14:49:42 +0000

    fix: key's id name according to the DID Peer new specs #126 (#148)
    Fix for #126
    
    Signed-off-by: Francisco Javier Ribó Labrador <elribonazo@gmail.com>
    Signed-off-by: Francisco Javier Ribo Labrador <elribonazo@gmail.com>

--- tests/agent/mocks/CastorMock.ts
+++ tests/agent/mocks/CastorMock.ts
@@ -12,7 +12,7 @@ const castorVars = {
   _peerDID: new DID(
     "did",
     "peer",
-    "2.Ez6LSms555YhFthn1WV8ciDBpZm86hK9tp83WojJUmxPGk1hZ.Vz6MkmdBjMyB4TS5UbbQw54szm8yvMMf1ftGV2sQVYAxaeWhE.SeyJpZCI6Im5ldy1pZCIsInQiOiJkbSIsInMiOiJodHRwczovL21lZGlhdG9yLnJvb3RzaWQuY2xvdWQiLCJhIjpbImRpZGNvbW0vdjIiXX0"
+    "2.Ez6LSms555YhFthn1WV8ciDBpZm86hK9tp83WojJUmxPGk1hZ.Vz6MkmdBjMyB4TS5UbbQw54szm8yvMMf1ftGV2sQVYAxaeWhE.SeyJpZCI6Im5ldy1pZCIsInQiOiJkbSIsInMiOnsidXJpIjoiaHR0cHM6Ly9tZWRpYXRvci5yb290c2lkLmNsb3VkIiwiYSI6WyJkaWRjb21tL3YyIl19fQ"
   ),
 }
 
