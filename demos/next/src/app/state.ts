86dece458249b01357493146c557f65eb7a2c917
Javier Ribó <elribonazo@gmail.com>
2024-02-27 12:23:01 +0100

    fix: add reference app + mediator live mode (#177)
    Signed-off-by: FabioPinheiro <fabiomgpinheiro@gmail.com>
    Co-authored-by: FabioPinheiro <fabiomgpinheiro@gmail.com>

--- demos/next/src/app/state.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "jotai";
-import { Domain } from "@atala/prism-wallet-sdk";
-
-export const mnemonicsAtom = atom<Domain.MnemonicWordList | undefined>(
-  undefined
-);
---
+++ demos/next/src/pages/debug.tsx
@@ -0,0 +1,50 @@
+
+import React, { useState } from "react";
+import SDK from "@atala/prism-wallet-sdk";
+import '../app/index.css'
+import { FooterNavigation } from "@/components/FooterNavigation";
+import { Mnemonics } from "@/components/Mnemonics";
+import { KeyPair } from "@/components/Keypair";
+import { Dids } from "@/components/Dids";
+import { Box } from "@/app/Box";
+import { useMountedApp } from "@/reducers/store";
+
+
+export default function App() {
+  const app = useMountedApp();
+  const [mediatorDID, setMediatorDID] = useState<string>(app.mediatorDID)
+
+  function onChangeMediatorDID(e) {
+    setMediatorDID(e.target.value)
+  }
+
+  return (
+    <>
+      <div className="mx-10 mt-5 mb-30">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          Debug
+        </h1>
+        <Box>
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            Settings
+          </h1>
+
+          <label htmlFor="mediatordid">MediatorDID</label>
+          <input
+            id="mediatordid"
+            value={mediatorDID}
+            onChange={onChangeMediatorDID}
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </Box>
+        <Mnemonics />
+        <KeyPair curve={SDK.Domain.Curve.SECP256K1} />
+        <KeyPair curve={SDK.Domain.Curve.ED25519} />
+        <KeyPair curve={SDK.Domain.Curve.X25519} />
+        <Dids />
+        <br /> <br /> <br /> <br /> <br /> <br />
+      </div>
+      <FooterNavigation />
+    </>
+  );
+}
