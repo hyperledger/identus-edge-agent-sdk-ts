5626e29235c045355872604dba47b08cbb14ec93
Francisco Javier Ribo Labrador <elribonazo@gmail.com>
2024-06-25 20:20:52 +0200

    fix: e2e issues fix Signed-off-by: Francisco Javier Ribó Labrador <elribonazo@gmail.com>

--- postinstall.sh
+++ postinstall.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-FILE="./node_modules/rxdb"
+FILE="$PWD/node_modules/rxdb"
+echo "running postinstall in $PWD"
 set -e
-# Patching RXDB package for 2 main reasons
-# 1. Its using crypto-js which is an insecure dependency we don't want to even
-# include in our package-locks, its 100% not used as we have created as we have
-# created our own package
-# 2. The second one is around a replication package that rxdb uses
-# firebase which includes a medium severity vuleranility which we also don't
-# want to be including in our package locks, despite 100% not being used.
-# Workaround: We install 
-rm -rf ./package-lock.json
-rm -rf ./node_modules/.package-lock.json
+rm -rf $PWD/package-lock.json
+rm -rf $PWD/node_modules/.package-lock.json
 npm i --ignore-scripts
