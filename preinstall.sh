ba908b15338e9dd52c2d88597f0a1a86c827d198
Javier Ribó <elribonazo@gmail.com>
2024-06-25 20:33:03 +0200

    fix: e2e issues fix (#236)

--- preinstall.sh
+++ preinstall.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-FILE="./node_modules/rxdb"
+FILE="$PWD/node_modules/rxdb"
+echo "running preinstall in $PWD"
 set -e
-
-
-
-
-if [ -z "./node_modules/rxdb/patched/done" ]; then
+if [ -z "$PWD/node_modules/rxdb/patched" ]; then
     echo "preinstall completed"
 else 
     npm i rxdb@14.17.1 --ignore-scripts
     npx patch-package
-    touch ./node_modules/rxdb/patched/done
+    touch $PWD/node_modules/rxdb/patched/done
 fi
-
-
-
-
-
-
-
