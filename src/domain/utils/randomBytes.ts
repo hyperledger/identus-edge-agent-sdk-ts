fabcc2c636b237414ad4d0b27717c9994bd1b9ca
Curtis <curtis.harding@iohk.io>
2024-09-03 19:13:09 +0100

    fix: noble-ciphers dependency (#284)
    Signed-off-by: Curtish <ch@curtish.me>

--- src/domain/utils/randomBytes.ts
+++ src/domain/utils/randomBytes.ts
@@ -1,8 +1,8 @@
-import { crypto } from '@noble/ciphers/webcrypto/crypto';
+import { crypto } from '@noble/ciphers/crypto';
 
 export function randomBytes(bytes: Uint8Array): Uint8Array {
-    if (crypto && typeof crypto.getRandomValues === 'function') {
-        return crypto.getRandomValues(bytes);
-    }
-    throw new Error('crypto.getRandomValues must be defined');
+  if (crypto && typeof crypto.getRandomValues === 'function') {
+    return crypto.getRandomValues(bytes);
+  }
+  throw new Error('crypto.getRandomValues must be defined');
 }
