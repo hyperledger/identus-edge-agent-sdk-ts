bb6c01377b9a7db0bf1fdadb7cb64333fc24296b
Curtish <ch@curtish.me>
2025-01-03 11:25:45 +0000

    feat!: updating to reusable Startable class
    Signed-off-by: Curtish <ch@curtish.me>

--- src/domain/buildingBlocks/Pluto.ts
+++ src/domain/buildingBlocks/Pluto.ts
@@ -8,6 +8,7 @@ import { PeerDID } from "../../peer-did/PeerDID";
 import { uuid } from "@stablelib/uuid";
 import { Arrayable } from "../../utils";
 import * as Backup from "../backup";
+import { Startable } from "../protocols/Startable";
 
 export namespace Pluto {
   /**
@@ -32,19 +33,7 @@ export namespace Pluto {
  * which will be implemented using this SDK. Implement this interface using your
  * preferred underlying storage technology, most appropriate for your use case.
  */
-export interface Pluto {
-  // TODO extend Startable.Controller (changes the interface)
-
-  /**
-   * Handle startup.
-   */
-  start(): Promise<void>;
-
-  /**
-   * Handle teardown.
-   */
-  stop?(): Promise<void>;
-
+export interface Pluto extends Startable.IController {
   /**
    * create a Backup object from the stored data
    */
