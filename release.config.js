f0d692142e46d79e3b28de1aab285e9527bbfd25
Allain Magyar <allain.magyar@iohk.io>
2025-01-30 09:28:11 -0300

    partial work
    Signed-off-by: Allain Magyar <allain.magyar@iohk.io>

--- release.config.js
+++ release.config.js
@@ -1,15 +1,11 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
 module.exports = {
-  branches: [
-    'main',
-    'release/*',
-    {
-      name: 'rc',
-      prerelease: true
-    }
-  ],
+  branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    ['@semantic-release/commit-analyzer', { preset: "conventionalcommits" }],
+    ['@semantic-release/release-notes-generator', { preset: "conventionalcommits" }],
     ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
     ['@semantic-release/exec', { "prepareCmd": './publish.sh ${nextRelease.version}' }],
     [
@@ -24,17 +20,5 @@ module.exports = {
         message: 'chore(release): release ${nextRelease.version}\n\n${nextRelease.notes}',
       },
     ],
-    // [
-    //     'semantic-release-slack-bot',
-    //     {
-    //         notifyOnSuccess: true,
-    //         notifyOnFail: true,
-    //         markdownReleaseNotes: true,
-    //         onSuccessTemplate: {
-    //             text:
-    //                 "A new version of `$package_name` successfully released!\nVersion: `$npm_package_version`\nTag: $repo_url/releases/tag/v$npm_package_version\n\nRelease notes:\n$release_notes",
-    //         },
-    //     },
-    // ],
   ],
 };
