f6e29988093c0ae76f06eeab3e76063413368a55
Curtish <ch@curtish.me>
2025-03-04 11:13:26 +0000

    feat!: updating DIDComm other protocols
    Signed-off-by: Curtish <ch@curtish.me>

--- src/edge-agent/protocols/types.ts
+++ src/edge-agent/protocols/types.ts
@@ -19,32 +19,8 @@ export interface PickupRequestBody {
   limit: number;
 }
 
-export interface PickupAttachment {
-  attachmentId: string;
-  data: string;
-}
-
-export interface PrismOnboardingInvitationBody {
-  type: string;
-  onboardingEndpoint: string;
-  from: string;
-}
-
-export interface BasicMessageBody {
-  content: string;
-}
-
-export interface ProblemReportBody {
-  code: string,
-  comment: string,
-  args: string[],
-  escalate_to: string;
-}
-
 export interface HandshakeRequestBody {
   goalCode?: string;
   goal?: string;
   accept?: string[];
 }
-
-export type OutOfBandInvitationBody = HandshakeRequestBody;
