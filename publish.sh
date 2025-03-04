966e04ee4b9d4ba9d1e404c4d3d062abcf854530
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2025-03-04 20:37:48 +0800

    build: modify release.config.js and publish.sh to switch the package name to @hyperledger/identus-sdk
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- publish.sh
+++ publish.sh
@@ -2,12 +2,43 @@
 
 set -e
 
-if [ -z "$1" ]; then
-    echo "Error: Missing release_version parameter. Please check what happened."
+# Default values
+dry_run=false
+release_version=""
+
+# Parse named arguments --version and --dry-run
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --version)
+            if [ -n "$2" ] && [[ "$2" != "--"* ]]; then
+                release_version="$2"
+                shift 2
+            else
+                echo "Error: --version requires a value."
+                exit 1
+            fi
+            ;;
+        --dry-run)
+            dry_run=true
+            shift
+            ;;
+        *)
+            echo "Error: Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+# Ensure the required --version argument is set
+if [ -z "$release_version" ]; then
+    echo "Error: Missing required argument --version {version}"
     exit 1
 fi
 
-release_version="$1"
+echo "Release version: $release_version"
+if $dry_run; then
+    echo "Running in dry run mode. No changes will be made."
+fi
 
 # Updates the version to the release
 npm version "$release_version" --git-tag-version false
@@ -16,17 +47,30 @@ npm version "$release_version" --git-tag-version false
 npm run build
 
 # Gets the published versions in the registry
-version_list=$(npm view @hyperledger/identus-edge-agent-sdk versions)
-published_versions=${version_list//[\[\]]/}
+old_version_list=$(npm view @hyperledger/identus-edge-agent-sdk versions)
+
+# The package @hyperledger/identus-sdk might not exist in the first release
+if ! version_list=$(npm view @hyperledger/identus-sdk versions 2>/dev/null); then
+  echo "Warning: Package @hyperledger/identus-sdk not found. Proceeding without new version list."
+  version_list=""
+fi
+
+# Merge both version lists, remove brackets and duplicates
+merged_versions=$(echo "$old_version_list $version_list" | tr -d '[],')
+unique_versions=$(echo "$merged_versions" | tr ' ' '\n' | sort -u | tr '\n' ' ')
 
 # Checks if it's been already published to npmjs
-if [[ ${published_versions[@]} =~ "'$release_version'" ]]; then
+if [[ ${unique_versions} =~ "'$release_version'" ]]; then
     # The goal of this case is to enable the generation of semantic-release
     # commits despite of the current version is already published.
     # Usually this is due some error during the pipeline execution.
     echo "$release_version is already published. Skipping publication."
 else
-    npm publish --access public
+    if $dry_run; then
+        echo "Dry run: Skipping npm publish for the version $release_version."
+    else
+        npm publish --access public
+    fi
 fi
 
 
